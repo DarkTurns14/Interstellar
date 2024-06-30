d84898c905e32ebdc65eaf8e4ee7e20e44a59b82
Kayden <160945338+DarkTurns14@users.noreply.github.com>
2024-06-29 21:29:46 -0400

    Logins

--- config.js
+++ config.js
@@ -1,8 +1,20 @@
 const config = {
-  challenge: false, // Set to true if you want to enable password protection.
+  challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    interstellar: "password",
+  Calum: "Smile3828",
+  Dean: "AddictedToLegos",
+  Emanuel: "24689",
+  Emma: "iloveketchup1526",
+  Jade: "st4rr22",
+  Jadiel: "scented0candle.",
+  Jasmine: "IdkMisi",
+  Jayslee: "G3NSH1N_24",
+  Kale: "KaColon09",
+  Quentin: "Applesauce",
+  Xzavier: "Xzavier13",
+  Zavian: "Bruvidk_4410",
+  OwnerKey: "4523",
   },
 };
 export default config;
