0b57087a1c513c6d06bfd8a254623b659a3c2e66
Kayden <160945338+DarkTurns14@users.noreply.github.com>
2024-09-03 13:42:43 +0000

    Logins

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-  challenge: false, // Set to true if you want to enable password protection.
+  challenge: true, // Set to true if you want to enable password protection.
   blocked: {
     // Normal users can ignore this option. This is for the owner of the website.
     "gointerstellar.app": "",
@@ -7,7 +7,19 @@ const config = {
   },
   users: {
     // You can add multiple users by doing username: 'password'.
-    interstellar: "password",
+  Calum: "Smile3828",
+  Dean: "AddictedToLegos",
+  Emanuel: "24689",
+  Emma: "iloveketchup1526",
+  Jade: "st4rr22",
+  Jadiel: "scented0candle.",
+  Jasmine: "IdkMisi",
+  Jayslee: "G3NSH1N_24",
+  Kale: "KaColon09",
+  Quentin: "Applesauce",
+  Xzavier: "Xzavier13",
+  Zavian: "Bruvidk_4410",
+  OwnerKey: "4523",
   },
 };
 
