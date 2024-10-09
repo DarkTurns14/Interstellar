e5e87b4b4022ee1f87ae09b63347bcce279fde0c
Kayden <160945338+Arkhaviss@users.noreply.github.com>
2024-10-09 17:02:49 -0400

    Mass removal of passes

--- config.js
+++ config.js
@@ -8,17 +8,7 @@ const config = {
     // You can add multiple users by doing username: 'password'.
   Arkhavis: "m10142010m",
   Agnes: "_C0d3n@m3_S0l1t@1r3_",
-  Calum: "Smile3828",
-  Dean: "AddictedToLegos",
-  Emanuel: "24689",
-  Emma: "iloveketchup1526",
-  Jade: "st4rr22",
-  Jasmine: "IdkMisi",
   Jayslee: "G3NSH1N_24",
-  Kale: "KaColon09",
-  Quentin: "Applesauce",
-  Xzavier: "Xzavier13",
-  Zavian: "Bruvidk_4410"
   },
 };
 
